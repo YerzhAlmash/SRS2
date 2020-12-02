7ce45055ec1fc0a5d5a03610ed055b86b6e6f36a
YerzhAlmash <74873242+YerzhAlmash@users.noreply.github.com>
2020-12-02 06:42:19 +0600

    Add files via upload

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+namespace Shop.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Name = "Almash";
+        public string City = "Oral";
+        public string NameState = "";
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+        public void OnPost(string n, string c)
+        {
+
+            if (n.Equals(Name) && c.Equals(City))
+            {
+                NameState = "OK";
+                HttpContext.Session.SetString("Name", "Almash");
+                HttpContext.Session.SetString("City", "Oral");
+
+            }
+            else
+            {
+                NameState = "ERROR";
+            }
+        }
+        public IActionResult OnPostLogout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToPage("Index");
+        }
+    }
+}
---
+++ Pages/NewRazor.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Shop.Models;
+
+namespace Shop.Pages
+{
+    public class NewRazorModel : PageModel
+    {
+        private readonly Shop.Models.SRSDBContext _context;
+
+        public NewRazorModel(Shop.Models.SRSDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Books.ToListAsync();
+        }
+    }
+}
